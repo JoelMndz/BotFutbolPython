2cf52e5eb308f496cc256760c8941d2276319e74
JoelMndz <luisjo3lml@gmail.com>
2022-04-21 12:22:13 -0500

    definir arquitectura

---
+++ vista/Ventana.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+from tkinter import END, ttk
+from tkinter.messagebox import showerror
+
+class Ventana(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.configurar_ventana()
+        self.cargar_componentes()
+
+    def configurar_ventana(self):
+        # Tamaño
+        self.geometry('690x360')
+        self.resizable(0, 0)
+        self.configure(bg='#0489B1')
+        self.title('La Liga Bot')
+
+    def cargar_componentes(self):
+        print('OK')
+
+
+v= Ventana()
+v.mainloop()
